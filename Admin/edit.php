8fdbec4a9fc2f8319cbff0684b0a274e5b6c7fd7
deepak21210 <deepakanandhan28@gmail.com>
2024-03-12 12:03:13 +0530

    Fix : Title Changed

--- Admin/edit.php
+++ Admin/edit.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editing Event</title>
+    <title>Edit Events</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <style>
