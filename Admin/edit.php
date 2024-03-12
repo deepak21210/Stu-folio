6392cd4659fa9e22ca4887ae0319586185b2c18c
Deepak <147963309+deepak21210@users.noreply.github.com>
2024-03-12 12:00:05 +0530

    Update edit.php

--- Admin/edit.php
+++ Admin/edit.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Events</title>
+    <title>Editing Event</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <style>
